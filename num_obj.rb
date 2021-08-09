33c90574d812a3094fd46dc58ca1ce389a341d2b
takahirokaji <tk69kj619@gmail.com>
2021-08-09 14:49:04 +0900

    good job

---
+++ num_obj.rb
@@ -0,0 +1,20 @@
+#数値
+
+
+# p 4.8.class
+# p 4.8.methods
+
+# p 10 + 3
+# p 10 * 3
+# p 2.4 * 2
+# p 10 / 3
+# p 10 % 3
+# p 10.0 / 3
+# p Rational(2,5) + Rational(3, 4);
+# p 2/5r + 3/4r;
+
+
+msg = 2.7777
+p msg.round;
+p msg.floor;
+p msg.ceil;
